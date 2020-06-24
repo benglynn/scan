54de7ae2aab24a57d1b93b369bc3f165eb4afa1c
Ben Glynn <ben@benedictglynn.com>
2020-06-24 10:13:50 +0100

    Linting (#8)

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,15 +1,20 @@
-const path = require('path');
-const fs = require('fs');
-const { createScan } = require('../src');
-const spec = require('../test/fixtures/image-spec');
-const filePth = path.join(__dirname,
-    '../test/fixtures/christopher-burns-Kj2SaNHG-hg-unsplash.jpg');
+const path = require("path");
+const fs = require("fs");
+const { createScan } = require("../src");
+const spec = require("../test/fixtures/image-spec");
+const filePth = path.join(
+  __dirname,
+  "../test/fixtures/christopher-burns-Kj2SaNHG-hg-unsplash.jpg"
+);
 
 const generator = createScan(spec);
 
 Promise.all(generator.process(filePth))
-    .then(assets => assets.forEach(asset => {
-        const pth = path.join(__dirname, '../dist', asset.name);
-        fs.writeFileSync(pth, asset.source());
-        console.log(`Created ${pth}`);
-    })).catch(error => console.error(error));
+  .then((assets) =>
+    assets.forEach((asset) => {
+      const pth = path.join(__dirname, "../dist", asset.name);
+      fs.writeFileSync(pth, asset.source());
+      console.log(`Created ${pth}`);
+    })
+  )
+  .catch((error) => console.error(error));
--- test/scan.spec.js
+++ test/scan.spec.js
@@ -1,83 +1,78 @@
-const expect = require('chai').expect;
-const { createScan } = require('../src');
-const specFixture = require('./fixtures/image-spec.json');
+const expect = require("chai").expect;
+const { createScan } = require("../src");
+const specFixture = require("./fixtures/image-spec.json");
 
-describe('Scan', function () {
+describe("Scan", function () {
+  const createStubScan = (spec) => {
+    const readFileSync = () => 12345;
+    const md5 = () => "mockhash";
+    const sharp = () => ({
+      resize: () => ({
+        toFormat: () => ({
+          toBuffer: () => Promise.resolve({ byteLength: 999 }),
+        }),
+      }),
+    });
+    const options = { readFileSync, md5, sharp };
+    return createScan(spec, options);
+  };
 
-    const createStubScan = spec => {
-        const readFileSync = () => 12345;
-        const md5 = () => 'mockhash';
-        const sharp = () => ({
-            resize: () => ({
-                toFormat: () => ({
-                    toBuffer: () => Promise.resolve({ byteLength: 999 })
-                })
-            })
-        });
-        const options = { readFileSync, md5, sharp };
-        return createScan(spec, options);
-    };
-
-    describe('create', function () {
-
-        it('should throw for an invalid spec', function () {
-            const message = 'unexpected spec format';
-            expect(createStubScan).to.throw(message);
-            expect(() => createStubScan('xxx')).to.throw(message);
-            expect(() => createStubScan(['invalid', 'invalid'])).to.throw(message);
-            expect(() => createStubScan(['1x1-160w.svg'])).to.throw(message);
-        });
-
-        it('should create', function () {
-            expect(() => createStubScan(['1x1-160w.jpg'])).not.to.throw();
-            expect(() => createStubScan(specFixture)).not.to.throw();
-        });
+  describe("create", function () {
+    it("should throw for an invalid spec", function () {
+      const message = "unexpected spec format";
+      expect(createStubScan).to.throw(message);
+      expect(() => createStubScan("xxx")).to.throw(message);
+      expect(() => createStubScan(["invalid", "invalid"])).to.throw(message);
+      expect(() => createStubScan(["1x1-160w.svg"])).to.throw(message);
     });
 
-    describe('parseSpec', function () {
+    it("should create", function () {
+      expect(() => createStubScan(["1x1-160w.jpg"])).not.to.throw();
+      expect(() => createStubScan(specFixture)).not.to.throw();
+    });
+  });
 
-        it('parses each passed spec', function () {
-            const processor = createStubScan(specFixture);
-            expect(processor.spec).to.have.lengthOf(12);
-        });
-
-        it('parses each attribute correctly', function () {
-            const processor = createStubScan(['8x5-1360w.webp']);
-            const parsed = processor.spec[0];
-            expect(parsed.name).to.equal('8x5-1360w.webp');
-            expect(parsed.width).to.equal(1360);
-            expect(parsed.height).to.equal(1360 * 5 / 8);
-            expect(parsed.type).to.equal('webp');
-        });
-
+  describe("parseSpec", function () {
+    it("parses each passed spec", function () {
+      const processor = createStubScan(specFixture);
+      expect(processor.spec).to.have.lengthOf(12);
     });
 
-    describe('process', function () {
+    it("parses each attribute correctly", function () {
+      const processor = createStubScan(["8x5-1360w.webp"]);
+      const parsed = processor.spec[0];
+      expect(parsed.name).to.equal("8x5-1360w.webp");
+      expect(parsed.width).to.equal(1360);
+      expect(parsed.height).to.equal((1360 * 5) / 8);
+      expect(parsed.type).to.equal("webp");
+    });
+  });
 
-        it('should throw for an unsuported file type', function () {
-            const instance = createStubScan(specFixture);
-            const message = /^unexpected file path/;
-            expect(() => instance.process('not a file')).to.throw(message);
-            expect(() => instance.process('/path/to/unsuported.svg')).to.throw(message);
-            expect(() => instance.process('no-path.jpg')).not.to.throw();
-        });
-
-        it('should return an array of promises', function () {
-            const instance = createStubScan(specFixture);
-            const result = instance.process('file.jpg');
-            expect(result).to.be.an('array');
-            expect(result[0]).to.be.a('promise');
-        });
-
-        it('should return a promise that resolves to spec', async function () {
-            const instance = createStubScan(['8x5-1360w.webp']);
-            const result = await instance.process('file-name.jpg')[0];
-            expect(result.source).to.be.a('function');
-            expect(result.source().byteLength).to.equal(999);
-            expect(result.size).to.be.a('function');
-            expect(result.name).to.equal('file-name-mockhash-8x5-1360w.webp');
-        });
-
+  describe("process", function () {
+    it("should throw for an unsuported file type", function () {
+      const instance = createStubScan(specFixture);
+      const message = /^unexpected file path/;
+      expect(() => instance.process("not a file")).to.throw(message);
+      expect(() => instance.process("/path/to/unsuported.svg")).to.throw(
+        message
+      );
+      expect(() => instance.process("no-path.jpg")).not.to.throw();
     });
 
+    it("should return an array of promises", function () {
+      const instance = createStubScan(specFixture);
+      const result = instance.process("file.jpg");
+      expect(result).to.be.an("array");
+      expect(result[0]).to.be.a("promise");
+    });
+
+    it("should return a promise that resolves to spec", async function () {
+      const instance = createStubScan(["8x5-1360w.webp"]);
+      const result = await instance.process("file-name.jpg")[0];
+      expect(result.source).to.be.a("function");
+      expect(result.source().byteLength).to.equal(999);
+      expect(result.size).to.be.a("function");
+      expect(result.name).to.equal("file-name-mockhash-8x5-1360w.webp");
+    });
+  });
 });
